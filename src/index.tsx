c9b71e6b6eb5f0d598fdcd4916a8a1b5334ab9e0
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-13 21:59:55 -0700

    create a store and provide it to App component using redux and thunk

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
