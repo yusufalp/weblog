897597ac0eb3219a8255f5409483da5b17ba9619
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-14 09:51:25 -0700

    import and display posts from single user with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import PostUsers from "./components/PostUsers/PostUsers";
+import User from "./components/User/User";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">We-Blog</header>
       <PostUsers />
+      <User />
     </div>
   );
 }
