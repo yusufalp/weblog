3a3e7fc6011d3dc0ea10b4edbc40ace823ddd832
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-13 21:59:03 -0700

    implement redux to fetch users from the API endpoint

--- src/components/PostUsers/PostUsers.tsx
+++ src/components/PostUsers/PostUsers.tsx
@@ -1,29 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../../actions";
 
-const PostUsers = () => {
-  return (
-    <div>
-      <h2>List of Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">City</th>
-            <th scope="col">Company</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Test</td>
-            <td>test@example.com</td>
-            <td>Test City</td>
-            <td>Test Company</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+class PostUsers extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  render() {
+    console.log(this.props.users);
+    return (
+      <div>
+        <h2>List of Users</h2>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">City</th>
+              <th scope="col">Company</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Test</td>
+              <td>test@example.com</td>
+              <td>Test City</td>
+              <td>Test Company</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { users: state.users };
 };
 
-export default PostUsers;
+export default connect(mapStateToProps, { fetchUsers: fetchUsers })(PostUsers);
