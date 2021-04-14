60bf32a25399ffd4c7a3e52ba3bf5b0e444da5e2
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-14 10:07:00 -0700

    combine the userPostsReducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import usersReducer from "./usersReducer";
-import userPostsReducer from "./userReducer";
+import userPostsReducer from "./userPostsReducer";
 
 export default combineReducers({
   users: usersReducer,
