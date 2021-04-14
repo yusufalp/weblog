b9d634e64ed840a9a509cc51bc20efa90aad0f8e
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-14 09:53:56 -0700

    implement a fetch request for a single user

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -11,3 +11,13 @@ export const fetchUsers = () => {
     dispatch({ type: "FETCH_USERS", payload: response.data });
   };
 };
+
+export const fetchUser = () => {
+  return async (dispatch) => {
+    const response = await axios
+      .create({ baseURL: "https://jsonplaceholder.typicode.com/" })
+      .get("posts?userId=1");
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  };
+};
