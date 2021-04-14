d60ea3ac49b66a0f8937db98eaf2565a1e96b7a2
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-13 21:28:08 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">We-Blog</header>
+    </div>
+  );
+}
+
+export default App;
