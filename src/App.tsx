b40c3afba3c66db227d69d16425c7b5d3c141009
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-13 21:40:01 -0700

    import the PostUsers component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import PostUsers from "./components/PostUsers/PostUsers";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">We-Blog</header>
+      <PostUsers />
     </div>
   );
 }
