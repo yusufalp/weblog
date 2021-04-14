4928fef09660e9eb5881f185f828492b8effed56
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-14 10:43:54 -0700

    implement react route for separate components

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 import PostUsers from "./components/PostUsers/PostUsers";
 import User from "./components/User/User";
 
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">We-Blog</header>
-      <PostUsers />
-      <User />
+      <Route exact path="/" component={PostUsers} />
+      <Route path="/users/:id" component={User} />
     </div>
   );
 }
