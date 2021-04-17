e69170b4035b6fdae76b5616ed039d735b6af738
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-17 09:18:13 -0700

    import memory router to enable use of Link inside a Router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/We-Blog/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  );
+  const title = screen.getByText(/We-Blog/i);
+  expect(title).toBeInTheDocument();
 });
