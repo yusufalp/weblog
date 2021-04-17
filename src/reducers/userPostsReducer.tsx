434ccd683c1156eb552e2aafb2d331de73afd3a9
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-17 09:19:52 -0700

    update the switch case type to match with action type

--- src/reducers/userPostsReducer.tsx
+++ src/reducers/userPostsReducer.tsx
@@ -1,6 +1,6 @@
 const userPostsReducer = (state = [], action) => {
   switch (action.type) {
-    case "FETCH_USER":
+    case "FETCH_USER_POSTS":
       return action.payload;
     default:
       return state;
