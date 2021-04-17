81d86f2b7dec21ff9037f3e41341c8816aacf7a1
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-17 09:21:28 -0700

    add a test file for component

---
+++ src/components/UserList/UserList.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UserList from "./UserList";
+
+import { render, fireEvent } from "@testing-library/react";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "../../reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+describe("Search input test", () => {
+  it("renders component correctly", () => {
+    let container: HTMLDivElement;
+    container = document.createElement("div");
+    ReactDOM.render(
+      <Provider store={store}>
+        <UserList />
+      </Provider>,
+      container
+    );
+  });
+
+  it("renders search input text correctly", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <UserList />
+      </Provider>
+    );
+    getByText("Search Name:");
+  });
+
+  it("allows users to search by name", () => {
+    const { getByLabelText } = render(
+      <Provider store={store}>
+        <UserList />
+      </Provider>
+    );
+    const input = getByLabelText("Search Name:");
+    fireEvent.change(input, { target: { value: "Leanne" } });
+  });
+});
