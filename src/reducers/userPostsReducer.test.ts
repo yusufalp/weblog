d291d29edd43523b2a81746b3d7e94f6ac2a68cb
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-18 08:48:14 -0700

    import action types

--- src/reducers/userPostsReducer.test.ts
+++ src/reducers/userPostsReducer.test.ts
@@ -1,4 +1,5 @@
 import userPostsReducer from "./userPostsReducer";
+import { FETCH_USER_POSTS } from "../actions/types";
 
 describe("User Post Reducer", () => {
   it("returns default state", () => {
@@ -8,12 +9,12 @@ describe("User Post Reducer", () => {
 
   it("returns a new state if received a valid type", () => {
     const posts = [
-      { title: "title 1", body: "body 1" },
-      { title: "title 2", body: "body 2" },
-      { title: "title 3", body: "body 3" },
+      { title: "title 1", body: "body 1", id: 1 },
+      { title: "title 2", body: "body 2", id: 2 },
+      { title: "title 3", body: "body 3", id: 3 },
     ];
     const newState = userPostsReducer(undefined, {
-      type: "FETCH_USER_POSTS",
+      type: FETCH_USER_POSTS,
       payload: posts,
     });
     expect(newState).toEqual(posts);
