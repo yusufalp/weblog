8e8291f1d12a0be6b790968918c79cbdd204cd64
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-18 08:44:48 -0700

    import action types

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
+import { Dispatch } from "redux";
+import { FETCH_USERS, FETCH_USER_POSTS, DispatchTypes } from "./types";
 
-export const fetchUsers = () => {
-  return async (dispatch) => {
-    const response = await axios
-      .create({
-        baseURL: "https://jsonplaceholder.typicode.com/",
-      })
-      .get("/users");
+export const fetchUsers = () => async (dispatch: Dispatch<DispatchTypes>) => {
+  const response = await axios
+    .create({
+      baseURL: "https://jsonplaceholder.typicode.com/",
+    })
+    .get("/users");
 
-    dispatch({ type: "FETCH_USERS", payload: response.data });
-  };
+  dispatch({ type: FETCH_USERS, payload: response.data });
 };
 
-export const fetchUserPosts = (id) => {
-  return async (dispatch) => {
-    const response = await axios
-      .create({ baseURL: "https://jsonplaceholder.typicode.com/" })
-      .get(`posts?userId=${id}`);
+export const fetchUserPosts = (id: number) => async (
+  dispatch: Dispatch<DispatchTypes>
+) => {
+  const response = await axios
+    .create({ baseURL: "https://jsonplaceholder.typicode.com/" })
+    .get(`posts?userId=${id}`);
 
-    dispatch({ type: "FETCH_USER_POSTS", payload: response.data });
-  };
+  dispatch({ type: FETCH_USER_POSTS, payload: response.data });
 };
