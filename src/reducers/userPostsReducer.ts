b4bfbf377c497ef20ab454c96f30e48827e8afbe
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-18 08:49:33 -0700

    assign type for state and action

--- src/reducers/userPostsReducer.ts
+++ src/reducers/userPostsReducer.ts
@@ -1,6 +1,19 @@
-const userPostsReducer = (state = [], action) => {
+import { FETCH_USER_POSTS, DispatchTypes } from "../actions/types";
+
+interface UserPost {
+  title: string;
+  body: string;
+  id: number;
+}
+
+type userPostsReducerDefaultState = UserPost[];
+
+const userPostsReducer = (
+  state: userPostsReducerDefaultState = [],
+  action: DispatchTypes
+) => {
   switch (action.type) {
-    case "FETCH_USER_POSTS":
+    case FETCH_USER_POSTS:
       return action.payload;
     default:
       return state;
